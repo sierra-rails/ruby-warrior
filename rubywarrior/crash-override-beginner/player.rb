0f09a3547abe2c5a7c78f70770cbbd4ebc6f2bce
Dave Paola <dpaola2@gmail.com>
2022-10-26 15:09:22 -0700

    dave level 5

--- rubywarrior/crash-override-beginner/player.rb
+++ rubywarrior/crash-override-beginner/player.rb
@@ -1,21 +1,50 @@
 class Player
   HEALTH_BUFFER = 20.freeze
+  attr_accessor :warrior
+
+  @prior_health = nil
+  @warrior = nil
 
   def play_turn(warrior)
-    next_space = warrior.feel
+    @warrior = warrior
 
-    if warrior.health < HEALTH_BUFFER
-      if next_space.empty?
-        warrior.rest!
-      else
-        warrior.attack!
-      end
+    if should_rest?
+      puts "Resting"
+      warrior.rest!
     else
-      if next_space.empty?
+      if should_walk?
+        puts "Walking"
         warrior.walk!
       else
+        puts "Attacking"
         warrior.attack!
       end
     end
+
+    @prior_health = warrior.health
+  end
+
+  def should_rest?
+    if taking_damage?
+      false
+    else
+      next_space.empty? && warrior.health < HEALTH_BUFFER
+    end
+  end
+
+  def should_walk?
+    next_space.empty?
+  end
+
+  def taking_damage?
+    if @prior_health.nil?
+      false # haven't started yet
+    else
+      warrior.health < @prior_health # current health is less than prior health
+    end
+  end
+
+  def next_space
+    warrior.feel
   end
 end

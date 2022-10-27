66bfe7f6305079aa739c2cbb5c530c3b3c0929e7
Andrew Neidley <neidz44@gmail.com>
2022-10-27 15:32:38 -0500

    reach lvl 4

--- rubywarrior/andrew-beginner/player.rb
+++ rubywarrior/andrew-beginner/player.rb
@@ -1,6 +1,14 @@
 class Player
   def play_turn(warrior)
     # add your code here
-    warrior.walk!
+    if warrior.feel.empty?
+      if warrior.health < 20
+        warrior.rest!
+      else
+        warrior.walk!
+      end
+    else
+      warrior.attack!
+    end
   end
 end

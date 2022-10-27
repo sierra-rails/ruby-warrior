3a9c694456c680a24fbe6b95eb542330d031de78
Andrew Neidley <neidz44@gmail.com>
2022-10-27 16:46:20 -0500

    reach lvl 5

--- rubywarrior/andrew-beginner/player.rb
+++ rubywarrior/andrew-beginner/player.rb
@@ -1,14 +1,47 @@
 class Player
   def play_turn(warrior)
     # add your code here
-    if warrior.feel.empty?
-      if warrior.health < 20
-        warrior.rest!
-      else
-        warrior.walk!
-      end
-    else
+    @health ||= 20
+
+    # if no one ahead, not taking damage, and wounded, HEAL
+    if warrior.feel.empty? && !(@health > warrior.health) && warrior.health < 20
+      warrior.rest!
+
+    # if enemy in range, ATTACK
+    elsif !warrior.feel.empty?
       warrior.attack!
+
+    # else WALK
+    else
+      warrior.walk!
     end
+
+    @health = warrior.health
+    puts "warrior health: #{warrior.health}"
+    puts "@health: #{@health}"
   end
+
+  # def enemy_in_range?
+  #   !warrior.feel.empty?
+  # end
+
+  # def enemy_out_of_range?
+  #   warrior.feel.empty?
+  # end
+
+  # def taking_damage?
+  #   @health <= warrior.health
+  # end
+
+  # def not_taking_damage?
+  #   @health < warrior.health
+  # end
+
+  # def wounded?
+  #   warrior.health < 20
+  # end
+
+  # def at_full_health?
+  #   warrior.health == 20
+  # end
 end
